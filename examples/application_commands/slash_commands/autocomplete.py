c3db158780accd3fb6f9cfd9f365214396056212
Chiggy-Playz <garg.chirag2004@gmail.com>
2022-02-09 00:19:30 +0530

    Use home made fake data instead of using faker

--- examples/application_commands/slash_commands/autocomplete.py
+++ examples/application_commands/slash_commands/autocomplete.py
@@ -1,49 +1,102 @@
 from typing import Any, Dict, List, Union
 
 import aiohttp
-import datetime
+from random import choice
+from itertools import product
 
 import discord
 from discord import app
 
-# https://github.com/joke2k/faker
-from faker import Faker
-
-
 client = discord.Client(intents=discord.Intents.none())
 
-# Create the faker object
-fake = Faker()
+# Generate some fake data
+first_names = [
+    "James",
+    "Robert",
+    "John",
+    "Michael",
+    "William",
+    "David",
+    "Mary",
+    "Patricia",
+    "Elizabeth",
+    "Susan",
+    "Barbara",
+]
+last_names = [
+    "Smith",
+    "Johnson",
+    "Williams",
+    "Jones",
+    "Brown",
+    "Davis",
+    "Miller",
+    "Wilson",
+    "Moore",
+    "Taylor",
+    "Anderson",
+]
+jobs = [
+    "Carpenter",
+    "Electrician",
+    "Teacher",
+    "Doctor",
+    "Lawyer",
+    "Accountant",
+    "Engineer",
+    "Programmer",
+    "Scientist",
+    "Artist",
+    "Nurse",
+]
+countries = [
+    "United States",
+    "United Kingdom",
+    "France",
+    "Germany",
+    "Japan",
+    "Russia",
+    "India",
+    "China",
+    "Australia",
+    "Canada",
+    "South Korea",
+]
 
-# Create some fake names
-fake_names = sorted([fake.unique.first_name() for _ in range(500)])
+profiles = {
+    f"{first_name} {last_name}": {
+        "job": choice(jobs),
+        "age": choice(range(18, 65)),
+        "sex": choice(["Male", "Female", "Other"]),
+        "country": choice(countries),
+    }
+    for first_name, last_name in product(first_names, last_names)
+}
 
 
 @client.application_command
-class RandomCommand(
-    app.SlashCommand,
-    name="person_info",
-    description="Sends information for a fake person."
-):
+class RandomCommand(app.SlashCommand, name="person_info", description="Sends information for a fake person."):
     # Use app.Option to enable autocomplete on an option by setting autocomplete=True
     # Only str, int, and float supports autocomplete.
     name: str = app.Option(description="Name of the person", autocomplete=True)
 
     async def callback(self):
-        
+
         # Get the name that the user typed
         name = self.name
-        
-        # Create a random fake profie
-        profile = fake.profile()
-
+        if name not in profiles:
+            await self.interaction.response.send_message("I was unable to find that person.")
+            return
+        profile = profiles[name]
         # Generate the fake information message
-        message = f"Here is the information for {name}:\n" + \
-                  f"**Sex:** {profile['sex']}\n" + \
-                  f"**Age:** {int((datetime.date.today()-profile['birthdate']).days / 365)}\n" + \
-                  f"**Job:** {profile['job']}\n" + \
-                  f"**Address:** {profile['residence']}\n"
-                
+        message = (
+            f"Here is the information for {name}:\n"
+            + f"**Sex:** {profile['sex']}\n"
+            + f"**Age:** {profile['age']}\n"
+            + f"**Job:** {profile['job']}\n"
+            + f"**Country:** {profile['country']}\n"
+        )
+
         # Send the message
         await self.interaction.response.send_message(message)
 
@@ -52,42 +105,38 @@ class RandomCommand(
     # focused is the name of the option that triggered the autocomplete
     # The method must return app.AutocompleteResult
     async def autocomplete(self, options: Dict[str, Union[int, float, str]], focused: str) -> app.AutoCompleteResponse:
-        
+
         # Create the response object
-        response = app.AutoCompleteResponse()        
+        response = app.AutoCompleteResponse()
 
         # Get the value that the user typed
         value = options[focused]
-        
+
         # Discord sends blank strings at the beginning of the autocomplete. So in that case we can return an empty response.
         if not value:
             return response
-    
+
         # Lowercase the value that user typed to prevent case sensitivity
         assert isinstance(value, str)
         value = value.lower()
-        
-        for name in fake_names:
-            
+
+        for name in profiles:
+
             if value in name.lower():
                 # Add the option to the response
                 # name is the value that the user will see
                 # value is the value that we will receive if user selects that option
                 response.add_option(name=name, value=name)
-                
+
                 # A maximum of 25 options can be returned
-                if len(response) == 25: 
+                if len(response) == 25:
                     break
 
         return response
-        
+
 
 @client.application_command
-class ColourCommand(
-    app.SlashCommand,
-    name="colour_info",
-    description="Sends information about a colour."
-):
+class ColourCommand(app.SlashCommand, name="colour_info", description="Sends information about a colour."):
     # Use app.Option to enable autocomplete on an option by setting autocomplete=True
     # Only str, int, and float supports autocomplete.
     colour: str = app.Option(description="The colour", autocomplete=True)
@@ -99,7 +148,6 @@ class ColourCommand(
         # We are using the Alexflipnote API to get some basic information about the colour
         # The colour endpoint to be precise.
         # https://alexflipnote.dev/colour/<hex_colour>
-
 
         # self.colour is the value that the user selected.
         # We need to remove the # from the colour so we can send it to the API.
@@ -113,10 +161,12 @@ class ColourCommand(
                 json_data: Dict[str, Any] = await response.json()
 
         # Construct the message with the information we got from the API.
-        message = f"🎨 **Information about {json_data['name']}**\n" + \
-                  f"**Hex:** {json_data['hex']}\n" + \
-                  f"**RGB:** {json_data['rgb']}\n" + \
-                  f"**Image:** {json_data['image']}\n"
+        message = (
+            f"🎨 **Information about {json_data['name']}**\n"
+            + f"**Hex:** {json_data['hex']}\n"
+            + f"**RGB:** {json_data['rgb']}\n"
+            + f"**Image:** {json_data['image']}\n"
+        )
 
         # Send the message.
         await self.interaction.followup.send(message)
@@ -128,7 +178,7 @@ class ColourCommand(
         # Get the value that the user typed.
         value = options[focused]
         assert isinstance(value, str)
-        
+
         # Create a list of random colours.
         # range(25) = loop 25 times.
         # _ is a placeholder for the value that we are not using.
@@ -139,7 +189,7 @@ class ColourCommand(
             # Add all the colours to the response.
             for colour in colours:
                 response.add_option(name=str(colour), value=str(colour))
-            
+
             # Return the response
             return response
 
@@ -149,7 +199,7 @@ class ColourCommand(
         for colour in colours:
             if str(value).strip("#") in str(colour)[1:]:
                 response.add_option(name=str(colour), value=str(colour))
-        
+
         # Return the response
         return response
 
